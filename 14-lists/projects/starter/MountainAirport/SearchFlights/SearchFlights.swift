54be1cb8ddf7afd92d77f1d9259320dbb8eaa5da
Đỗ Hoàng Sơn <sean.it.ios@gmail.com>
2025-03-15 02:03:44 +0700

    #15.4. Searchable Lists > Adding Final Search Touches > using .searchable prompt and .onChange

--- 14-lists/projects/starter/MountainAirport/SearchFlights/SearchFlights.swift
+++ 14-lists/projects/starter/MountainAirport/SearchFlights/SearchFlights.swift
@@ -177,7 +177,7 @@ struct SearchFlights: View {
                 Spacer()
             }
             // .searchable(text: $city)
-            .searchable(text: $city) {
+            .searchable(text: $city, prompt: "City Name") {
                 ForEach(FlightData.citiesContaining(city), id: \.self) { city in
                     Text(city).searchCompletion(city)
                 }
@@ -189,7 +189,16 @@ struct SearchFlights: View {
                     runningSearch = false
                 }
             }
-            
+            .onChange(of: city) { newText in
+                if newText.isEmpty {
+                    Task {
+                        runningSearch = true
+                        await flightData = FlightData.searchFlightsForCity(city)
+                        runningSearch = false
+                    }
+                }
+            }
+
             .navigationBarTitle("Search Flights")
             .padding()
         }
