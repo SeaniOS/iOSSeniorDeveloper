31734a54b1549e53e908bbe97c7439a940f4c4c3
Đỗ Hoàng Sơn <sean.it.ios@gmail.com>
2025-03-14 17:51:18 +0700

    #15.2 Pull to Refresh > using .refreshable

--- 14-lists/projects/starter/MountainAirport/FlightStatusBoard/FlightStatusBoard.swift
+++ 14-lists/projects/starter/MountainAirport/FlightStatusBoard/FlightStatusBoard.swift
@@ -29,60 +29,76 @@
 import SwiftUI
 
 struct FlightStatusBoard: View {
-    var flights: [FlightInformation]
+    @State var flights: [FlightInformation]
     var flightToShow: FlightInformation?
     @State private var hidePast = false
     @AppStorage("FlightStatusCurrentTab") var selectedTab = 1
     @State var highlightedIds: [Int] = []
-
+    
     var shownFlights: [FlightInformation] {
         hidePast ?
         flights.filter { $0.localTime >= Date() } :
         flights
     }
     
+    func lastUpdateString(_ date: Date) -> String {
+        let dateF = DateFormatter()
+        dateF.timeStyle = .short
+        dateF.dateFormat = .none
+        return "Last updated: \(dateF.string(from: Date()))"
+    }
+    
     var body: some View {
-        TabView(selection: $selectedTab) {
-            FlightList(
-                flights: shownFlights.filter { $0.direction == .arrival },
-                highlightedIds: $highlightedIds
-            )
-            .tabItem {
-                Image("descending-airplane")
-                    .resizable()
-                Text("Arrivals")
+        VStack {
+            Text(lastUpdateString(Date()))
+                .font(.footnote)
+            TabView(selection: $selectedTab) {
+                FlightList(
+                    flights: shownFlights.filter { $0.direction == .arrival },
+                    highlightedIds: $highlightedIds
+                )
+                .tabItem {
+                    Image("descending-airplane")
+                        .resizable()
+                    Text("Arrivals")
+                }
+                .tag(0)
+                FlightList(
+                    flights: shownFlights,
+                    flightToShow: flightToShow,
+                    highlightedIds: $highlightedIds
+                )
+                .tabItem {
+                    Image(systemName: "airplane")
+                        .resizable()
+                    Text("All")
+                }
+                .tag(1)
+                FlightList(
+                    flights: shownFlights.filter { $0.direction == .departure },
+                    highlightedIds: $highlightedIds
+                )
+                .tabItem {
+                    Image("ascending-airplane")
+                    Text("Departures")
+                }
+                .tag(2)
             }
-            .tag(0)
-            FlightList(
-                flights: shownFlights,
-                flightToShow: flightToShow,
-                highlightedIds: $highlightedIds
-            )
-            .tabItem {
-                Image(systemName: "airplane")
-                    .resizable()
-                Text("All")
+            .onAppear {
+                if flightToShow != nil {
+                    selectedTab = 1
+                }
             }
-            .tag(1)
-            FlightList(
-                flights: shownFlights.filter { $0.direction == .departure },
-                highlightedIds: $highlightedIds
-            )
-            .tabItem {
-                Image("ascending-airplane")
-                Text("Departures")
+            // 1
+            .refreshable {
+                // 2
+                await flights = FlightData.refreshFlights()
             }
-            .tag(2)
+            .navigationTitle("Today's Flight Status")
+            .navigationBarItems(
+                trailing: Toggle("Hide Past", isOn: $hidePast)
+            )
         }
-        .onAppear {
-            if flightToShow != nil {
-                selectedTab = 1
-            }
-        }
-        .navigationTitle("Today's Flight Status")
-        .navigationBarItems(
-            trailing: Toggle("Hide Past", isOn: $hidePast)
-        )
     }
 }
 
