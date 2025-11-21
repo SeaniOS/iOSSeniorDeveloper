41fbea0597ebeaf0fcb7e625263005df7fb63346
DO HOANG SON <sean.it.ios@gmail.com>
2025-11-21 15:08:17 +0700

    #flutter: - Declare and initialize variables

--- learning_flutter/lib/main.dart
+++ learning_flutter/lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  int age = 25;
+  double height = 5.9;
+  String name = "John Doe";
+  bool isStudent = true;
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height');
+  print('Is a Student: $isStudent');
   runApp(const MyApp());
 }
 
